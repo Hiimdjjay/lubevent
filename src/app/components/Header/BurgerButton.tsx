c2cd36dabbceee7a3d32d1a9ad5da3284d1147cb
Jakub <jakubrafalski96@gmail.com>
2026-03-03 19:49:37 +0000

    New nav

--- src/app/components/Header/BurgerButton.tsx
+++ src/app/components/Header/BurgerButton.tsx
@@ -6,12 +6,12 @@ export function BurgerButton() {
 
 	return (
 		<button
-			className='relative inline-flex justify-center items-center h-10 w-10 bg-white/10 rounded-full'
+			className={`relative inline-flex justify-center items-center h-10 w-10 bg-white/10 rounded-full cursor-pointer`}
 			onClick={() => setOpen(prev => !prev)}>
 			<span
-				className={`absolute -translate-y-1.25 bg-white w-5 h-0.5 rounded transition-transform ${open ? "rotate-45" : ''}`}></span>
+				className={`absolute -translate-y-1 bg-white w-5 h-0.5 rounded transition-all duration-200 ${open ? 'rotate-45' : ''}`}></span>
 			<span
-            {open && <p>burger</p>}
+				className={`absolute translate-y-1 bg-white w-5 h-0.5 rounded transition-all duration-200 ${open ? 'rotate-45' : ''}`}></span>
 		</button>
 	);
 }
